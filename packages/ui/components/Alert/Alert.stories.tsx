58fd87789ebc5c8803c0d3b745a0d7fa3a3aa340
mon <raquindoza14@gmail.com>
2021-12-26 14:25:50 +0800

    storybook

---
+++ packages/ui/components/Alert/Alert.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Alert } from "./Alert";
+
+export default {
+  title: "Example/Alert",
+  component: Alert,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof Alert>;
+
+const Template: ComponentStory<typeof Alert> = (args) => <Alert {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: "Button",
+};
---
+++ packages/ui/components/Alert/Alert.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { AlertProps } from "./Alert.types";
+
+export const Alert = (props: AlertProps) => {
+  return <div>this is an alert</div>;
+};
