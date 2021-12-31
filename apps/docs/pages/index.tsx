e21e1ddf79365b791c5c77d6677837e158a2d5e8
mon <raquindoza14@gmail.com>
2021-12-31 19:05:06 +0800

    test lint-staged
    test lint-staged
    
    test lint-staged
    
    test lint-staged
    
    test lint-staged
    
    test lint-staged

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Alert } from "ui";
 
 export default function Docs() {
-  console.log("hello");
   return (
     <div>
       <h1>Docs</h1>
--- apps/web/.prettierrc.js
+++ apps/web/.prettierrc.js
@@ -1,1 +1,1 @@
-module.exports = require("config/prettier-next");
+module.exports = require('config/prettier-next')
