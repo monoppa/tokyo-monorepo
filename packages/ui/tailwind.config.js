fccf0df03a939976c7d58c5ad591bca5c63d5f9b
mon <raquindoza14@gmail.com>
2022-01-01 13:26:48 +0800

    tailwind storybook
    tailwind storybook
    
    format before lint

---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// module.exports = require('tw-config/tailwind-config')
