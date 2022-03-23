ea5a477a92a62155a31df92fc108cafe8abfe057
mon <raquindoza14@gmail.com>
2022-03-23 15:37:36 +0800

    update packages and fix tailwind purging

--- packages/tw-config/tailwind-config.js
+++ packages/tw-config/tailwind-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/scenes/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "/packages/ui/components/**/*.{js,ts,jsx,tsx}",
   ],
