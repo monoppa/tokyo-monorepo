4aa494b8c46ac20f3ac92f66e0a3805cdae209a8
mon <raquindoza14@gmail.com>
2022-03-29 11:58:25 +0800

    add cron jobs implementation

--- apps/workers/src/worker.ts
+++ apps/workers/src/worker.ts
@@ -1,36 +1,41 @@
-import { run, Task, TaskList } from "graphile-worker";
+import { run, TaskList } from "graphile-worker";
 import { Pool } from "pg";
 import to from "await-to-js";
-
-const hello: Task = (payload: any, helpers) => {
-  helpers.logger.info("heloo", payload);
-};
+import refreshCoinPrice from "./tasks/refreshCoinPrice";
+import "dotenv/config";
 
 const worker = async () => {
   const rootPgPool = new Pool();
 
   const taskList: TaskList = {
-    hello,
+    refreshCoinPrice,
   };
 
   // Run a worker to execute jobs:
-  const [error] = await to(
+  const [error, runner] = await to(
     run({
+      crontabFile: "crontab",
       pgPool: rootPgPool,
       concurrency: 5,
       // Install signal handlers for graceful shutdown on SIGINT, SIGTERM, etc
       noHandleSignals: false,
       pollInterval: 1000,
       taskList,
-      // crontabFile: environment.CRONTAB_FILE,
     })
   );
 
   if (error) {
-    throw new Error(`Failed to initialize graphile-workers ${error}`);
+    throw new Error(
+      `Failed to initialize graphile-workers ${JSON.stringify(error)}`
+    );
   }
+
+  // If the worker exits (whether through fatal error or otherwise), this
+  // promise will resolve/reject:
+  await runner.promise;
 };
 
 worker().catch((err) => {
   console.error("Failed to start workers", err);
+  process.exit(1);
 });
